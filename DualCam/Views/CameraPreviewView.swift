ac3ea3fd71defde95748c93eb8cf1c9d80e72197
Aaron Park <apark@Aerons-Mac-mini.local>
2026-03-30 21:50:58 -0400

    Add crop guide overlay for Single Lens mode showing 16:9 landscape area

--- DualCam/Views/CameraPreviewView.swift
+++ DualCam/Views/CameraPreviewView.swift
@@ -23,6 +23,11 @@ class DualPreviewUIView: UIView {
     private var pipPreviewLayer: AVCaptureVideoPreviewLayer?
     private var pipLabel: UILabel?
     
+    // Single Lens crop guide overlay
+    private var cropGuideTop: UIView?
+    private var cropGuideBottom: UIView?
+    private var cropLabel: UILabel?
+    
     // PiP settings
     private var pipWidth: CGFloat = 120
     private var pipHeight: CGFloat = 68  // 16:9 default
@@ -34,10 +39,42 @@ class DualPreviewUIView: UIView {
         super.init(frame: frame)
         backgroundColor = .black
         setupPiPContainer()
+        setupCropGuides()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupCropGuides() {
+        // Semi-transparent overlays for top and bottom (areas that get cropped)
+        let topOverlay = UIView()
+        topOverlay.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        topOverlay.isHidden = true
+        topOverlay.isUserInteractionEnabled = false
+        addSubview(topOverlay)
+        cropGuideTop = topOverlay
+        
+        let bottomOverlay = UIView()
+        bottomOverlay.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        bottomOverlay.isHidden = true
+        bottomOverlay.isUserInteractionEnabled = false
+        addSubview(bottomOverlay)
+        cropGuideBottom = bottomOverlay
+        
+        // Label showing "16:9"
+        let label = UILabel()
+        label.text = "16:9 Landscape"
+        label.font = .systemFont(ofSize: 12, weight: .semibold)
+        label.textColor = .white
+        label.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        label.textAlignment = .center
+        label.layer.cornerRadius = 4
+        label.layer.masksToBounds = true
+        label.isHidden = true
+        label.isUserInteractionEnabled = false
+        addSubview(label)
+        cropLabel = label
     }
     
     private func setupPiPContainer() {
@@ -68,6 +105,41 @@ class DualPreviewUIView: UIView {
         mainPreviewLayer?.frame = bounds
         
         updatePiPLayout()
+        updateCropGuideLayout()
+    }
+    
+    private func updateCropGuideLayout() {
+        guard let camera = camera else { return }
+        
+        let isSingleLens = camera.recordingMode == .singleLens
+        
+        cropGuideTop?.isHidden = !isSingleLens
+        cropGuideBottom?.isHidden = !isSingleLens
+        cropLabel?.isHidden = !isSingleLens
+        
+        guard isSingleLens else { return }
+        
+        // Calculate 16:9 crop area
+        // Frame is 9:16 portrait, crop height = width * 9/16
+        let frameWidth = bounds.width
+        let cropHeight = frameWidth * 9.0 / 16.0
+        let cropY = (bounds.height - cropHeight) / 2.0
+        
+        // Top overlay (above crop area)
+        cropGuideTop?.frame = CGRect(x: 0, y: 0, width: bounds.width, height: cropY)
+        
+        // Bottom overlay (below crop area)
+        cropGuideBottom?.frame = CGRect(x: 0, y: cropY + cropHeight, width: bounds.width, height: bounds.height - cropY - cropHeight)
+        
+        // Label in center of crop area
+        let labelWidth: CGFloat = 100
+        let labelHeight: CGFloat = 24
+        cropLabel?.frame = CGRect(
+            x: (bounds.width - labelWidth) / 2,
+            y: cropY + cropHeight - labelHeight - 8,
+            width: labelWidth,
+            height: labelHeight
+        )
     }
     
     private func updatePiPLayout() {
@@ -105,6 +177,9 @@ class DualPreviewUIView: UIView {
     
     func updatePreviews() {
         guard let camera = camera else { return }
+        
+        // Update crop guides for single lens
+        updateCropGuideLayout()
         
         // Determine mode
         let isStreamerMode = camera.recordingMode == .streamer
